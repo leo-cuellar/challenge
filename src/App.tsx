3c555c50ee1f7b825d298393b158ae353b8d831e
leo-cuellar <52687532+leo-cuellar@users.noreply.github.com>
2021-09-07 13:49:00 -0500

    snackbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import generateMessage, { Message } from './Api'
+import Button from '@material-ui/core/Button'
+import Snackbar from '@material-ui/core/Snackbar'
+import IconButton from '@material-ui/core/IconButton'
+import CloseIcon from '@material-ui/icons/Close';
 
 import MessageTable from './components/MessageTable'
 import UserControls from './components/UserControls'
@@ -14,12 +18,22 @@ const AppContainer = styled.div`
 const App: React.FC<{}> = () => {
   const [messages, setMessages] = useState<Message[]>([])
   const [running, setRunning] = useState<boolean>(true)
+  const [snackBar, setSnackBar] = useState('')
+
+  const handleSnackBarClose = (event?: any, reason?: string) => {
+    if (reason === 'clickaway') {
+      return
+    }
+
+    setSnackBar('')
+  };
 
   useEffect(() => {
     let cleanUp: () => void = () => { }
     if (running) {
       cleanUp = generateMessage((message: Message) => {
         setMessages(oldMessages => [...oldMessages, message])
+        setSnackBar(message.message)
       })
     } else {
       cleanUp()
@@ -29,6 +43,23 @@ const App: React.FC<{}> = () => {
 
   return (
     <AppContainer>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        open={snackBar !== ''}
+        autoHideDuration={2000}
+        onClose={handleSnackBarClose}
+        message={snackBar}
+        action={
+          <React.Fragment>
+            <IconButton aria-label="close" color="inherit" onClick={handleSnackBarClose}>
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+      />
       <UserControls setMessages={setMessages} running={running} setRunning={setRunning} />
       <MessageTable messages={messages} setMessages={setMessages} />
     </AppContainer>
